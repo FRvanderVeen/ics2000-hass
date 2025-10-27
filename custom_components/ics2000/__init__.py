605d43a5004cf17a8f95e9375e4aab257a274137
Ferdinand van der Veen <86024496+FRvanderVeen@users.noreply.github.com>
2025-10-27 20:04:38 +0100

    Added config flow

--- custom_components/ics2000/__init__.py
+++ custom_components/ics2000/__init__.py
@@ -1,6 +1,22 @@
-DOMAIN = "ics2000"
+""" The KlikAanKlinUit integration """
+from __future__ import annotations
 
+import logging
 
-def setup(hass, config):  # noqa
-    # hass.states.set("hello_state.world", "Paulus")
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: list[Platform] = [
+    Platform.LIGHT
+]
+
+async def async_setup_entry(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up KlikAanKlikUit (ICS2000) from a config entry."""
+    _LOGGER.debug("Setting up KlikAanKlikUit (ICS2000) entry")
+
+    await hass.config_entries.async_forward_entry_setups(config, PLATFORMS)
+
     return True
---
+++ custom_components/ics2000/config_flow.py
@@ -0,0 +1,37 @@
+"""Config flow for the ICS2000 integration"""
+from __future__ import annotations
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_PASSWORD, CONF_MAC, CONF_EMAIL#, CONF_IP_ADDRESS
+from homeassistant.helpers import config_validation as cv
+
+from .const import DOMAIN, CONF_SLEEP, CONF_TRIES
+
+STEP_USER_DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_MAC): cv.string,
+    vol.Required(CONF_EMAIL): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_TRIES): cv.positive_int,
+    vol.Optional(CONF_SLEEP): cv.positive_int
+    # vol.Optional(CONF_IP_ADDRESS): cv.matches_regex(r'[1-9][0-9]{0,2}(\.(0|[1-9][0-9]{0,2})){2}\.[1-9][0-9]{0,2}'),
+    # vol.Optional(CONF_AES): cv.matches_regex(r'[a-zA-Z0-9]{32}')
+})
+
+class ICS2000ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """ICS2000 config flow."""
+    # The schema version of the entries that it creates
+    # Home Assistant will call your migrate method if the version changes
+    VERSION = 1
+    MINOR_VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+        )
